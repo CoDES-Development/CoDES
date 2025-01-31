9aad0a4ebeea3c7b24392be4a18a816ed0be796a
CoDES-Development <zhu_yuanyi1221@163.com>
2025-02-01 01:35:17 +0800

    RIP commit

---
+++ src/mpi-application/model/mpi-datatype.h
@@ -0,0 +1,28 @@
+
+
+#ifndef NS3_MPI_DATATYPE_H
+#define NS3_MPI_DATATYPE_H
+
+namespace ns3 {
+    struct FLOAT_INT{
+        float f;
+        int i;
+    };
+
+    struct DOUBLE_INT{
+        double d;
+        int i;
+    };
+
+    struct REPEATED_INT{
+        int i_1;
+        int i_2;
+    };
+
+    struct LONG_DOUBLE_INT{
+        long double ld;
+        int i;
+    };
+}
+
+#endif // NS3_MPI_DATATYPE_H
